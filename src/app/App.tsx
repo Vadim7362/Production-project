424a8b4b7ad42acb23284504e0d5afb7f1eca3b4
User <user@Users-MacBook-Pro.local>
2023-02-14 10:22:09 +0300

    Архитектура. Внедрение FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import {Route, Routes, Link,} from 'react-router-dom';
 import './styles/index.scss';
-import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
-import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import React, { Suspense, useContext, useState } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext';
-import { useTheme } from './theme/UseTheme';
-import { classNames } from './helpers/ClassNames/classNames';
+import { classNames } from 'shared/lib/helpers/ClassNames/classNames';
+import { Suspense } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 
 
 const App = () => {
 
 const {theme, toggleTheme} = useTheme();
-const bool = true;
 
   return (
     <div className= {classNames('app', {}, [theme])}>
@@ -21,8 +19,8 @@ const bool = true;
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />}/>
-          <Route path={'/'} element={<MainPageAsync />}/>
+          <Route path={'/about'} element={<AboutPage />}/>
+          <Route path={'/'} element={<MainPage />}/>
         </Routes>
       </Suspense>
     </div>
