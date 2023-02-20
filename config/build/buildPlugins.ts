9707cfb471cebad2b053677acf2369108a60b500
User <user@Users-MacBook-Pro.local>
2023-02-20 11:27:42 +0300

    i18n Интернационализация. Define plugin. Плагин для переводов

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 //import path from "path";
 
 
-export function buildPlugins ({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
   return [
     new HtmlWebpackPlugin({
@@ -16,5 +16,8 @@ export function buildPlugins ({paths}: BuildOptions): webpack.WebpackPluginInsta
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
     }),
+    new webpack.DefinePlugin( {
+      __IS_DEV__: JSON.stringify(isDev),
+    })
   ]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,7 @@ import { AppRouter } from './providers/router';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-
-
+import { Suspense } from 'react';
 
 const App = () => {
 
@@ -13,13 +12,13 @@ const App = () => {
 
   return (
     <div className= {classNames('app', {}, [theme])}>
-      <Navbar />
-
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
-
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
