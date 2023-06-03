33ae59e10d6bff3cb37e1890aedb05feefd4d884
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-06-03 21:25:47 +0300

    ErrorBoundary. Обработка React ошибок

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,11 @@ import { AppRouter } from 'app/providers/router';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 const App = () => {
 
-	const {theme} = useTheme();
+	const { theme } = useTheme();
 
 	return (
 		<div className= {classNames('app', {}, [theme])}>
