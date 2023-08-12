79fefa2952d75aaf5e6e761b8627debc0f4c0a28
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-08-12 21:12:01 +0300

    add login module

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,18 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userActions } from 'entities/User';
 
  function App() {
 
 
 	const { theme } = useTheme();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userActions.initAuthData());
+  }, [dispatch])
 
 	return (
 		<div className= {classNames('app', {}, [])}>
