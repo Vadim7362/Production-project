c51f4fa60432baca2a77a7a0b1b61863a3e7241c
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-08-23 23:36:22 +0300

    add new theme, add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,58 +1,41 @@
 import webpack from 'webpack';
 import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
 
-export function buildLoaders ({isDev}: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
 
-  const svgLoader = {
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  }
+    const babelLoader = buildBabelLoader(options);
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-        loader: "babel-loader",
-        options: {
-            presets: ['@babel/preset-env'],
-            "plugins": [
-                [
-                    "i18next-extract",
-                    {
-                        locales: ['ru', 'en'],
-                        keyAsDefaultValue: true
-                    }
-                ],
-            ]
-        }
-    }
-}
+    const cssLoader = buildCssLoader(isDev);
 
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif|woof2|woof)$/i,
-    use: [
-      {
-        loader: 'file-loader',
-      },
-    ],
-  }
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
 
-  const cssLoader = buildCssLoader(isDev);
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
 
-  // Если не используем typescript, нужен еще babel-loader
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-  }
-  
-  return [
-    fileLoader,
-    svgLoader,
-    babelLoader,
-    typescriptLoader,
-    cssLoader,
-  ]
+    return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
+        typescriptLoader,
+        cssLoader,
+    ];
 }
