653275ace092a4d6b682d1a1307331435c80721a
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-09-21 13:23:56 +0300

    add fetching articles, view type selector

--- src/pages/ArticlesPage/index.ts
+++ src/pages/ArticlesPage/index.ts
@@ -1,3 +1,5 @@
 export {
-    ArticlesPageAsync as ArticlesPage,
+  ArticlesPageAsync as ArticlesPage,
 } from './ui/ArticlesPage/ArticlesPage.async';
+
+export { ArticlesPageSchema } from './model/types/articlesPageSchema';
