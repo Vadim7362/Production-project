a7ea6dbed1baa721608ae25eea327bb3afdb72ea
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-10-19 21:58:25 +0300

    Подготовка к продакшну

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,5 @@
 import { lazy } from 'react';
 
-export const MainPageAsync = lazy(() => new Promise(resolve => {
-  // @ts-ignore
-  //Так в реальных проектах не делать!!!! Делаем только для КУРСА!!!!
-  setTimeout(() => resolve(import('./MainPage')), 1500)
-}));
+export const MainPageAsync = lazy(
+    () => import('./MainPage'),
+);
